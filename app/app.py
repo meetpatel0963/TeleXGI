1effa3993c28104929cc9ae9dc7f468e3fae6120
meetpatel0963 <18bce164@nirmauni.ac.in>
2024-03-29 15:44:18 +0530

    Added support for TeleXGI Analysis on uploaded and existing sample videos

--- app/app.py
+++ app/app.py
@@ -10,7 +10,8 @@ from PIL import Image
 
 from config import Config
 from models.resnet50 import ResNet50Classifier
-from utils.utils import make_prediction_image, format_filename
+from utils.utils import make_prediction_image, get_pil_image_from_frame, \
+                        format_filename, convert_to_mp4, process_video
 
 
 use_cuda = torch.cuda.is_available()
@@ -31,7 +32,7 @@ def main():
     st.title('TeleXGI: GastroIntestinal Disease Classification')
 
     # Upload image or video
-    uploaded_file = st.file_uploader("Upload an image or video", type=['jpg', 'jpeg', 'png'])
+    uploaded_file = st.file_uploader("Upload an image or video", type=['jpg', 'jpeg', 'png', 'mp4', 'avi'])
 
     # Example images
     example_images = []
@@ -39,30 +40,61 @@ def main():
         if os.path.isfile(os.path.join(Config.SAMPLE_IMAGES_PATH, filename)):
             example_images.append((format_filename(filename), os.path.join(Config.SAMPLE_IMAGES_PATH, filename)))  # Store tuple of (label, path)
 
+    # Example videos
+    example_videos = []
+    for filename in os.listdir(Config.SAMPLE_VIDEOS_PATH):
+        if os.path.isfile(os.path.join(Config.SAMPLE_VIDEOS_PATH, filename)):
+            example_videos.append((format_filename(filename), os.path.join(Config.SAMPLE_VIDEOS_PATH, filename)))  # Store tuple of (label, path)
+
     if uploaded_file is not None:
         file_extension = uploaded_file.name.split('.')[-1].lower()
+        if file_extension in ['mp4', 'avi']:
+            if file_extension == 'avi':
+                # Convert uploaded video to MP4 format
+                # video_path = convert_to_mp4(uploaded_file)
+                video_path = uploaded_file
 
-        # Display uploaded image
-        st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
+                # Process video and display frames with high confidence
+                process_video(video_path, model, test_transforms, device)
+        else:
+            # Display uploaded image
+            st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
 
-        # Convert uploaded image to PIL Image
-        pil_image = Image.open(uploaded_file)
+            # Convert uploaded image to PIL Image
+            pil_image = Image.open(uploaded_file)
 
-        # Make prediction on the image
-        predicted_class, confidence = make_prediction_image(pil_image, model, test_transforms, device)
-        st.write(f"Predicted class: {predicted_class}")
-        st.write(f"Confidence: {confidence}")
+            # Make prediction on the image
+            predicted_class, confidence = make_prediction_image(pil_image, model, test_transforms, device)
+            st.write(f"Predicted class: {predicted_class}")
+            st.write(f"Confidence: {confidence}")
 
-    selected_example_media = st.sidebar.selectbox("Select an example image", example_images, format_func=lambda x: x[0])
+    st.sidebar.title("Example Media")
+    selected_media_type = st.sidebar.radio("Select media type", ["Image", "Video"])
 
-    # Display example image
-    example_image = Image.open(selected_example_media[1])
-    st.sidebar.image(example_image, caption='Example Image', use_column_width=True)
+    if selected_media_type == "Image":
+        selected_example_media = st.sidebar.selectbox("Select an example image", example_images, format_func=lambda x: x[0])
 
-    # Make prediction on example image
-    predicted_class, confidence = make_prediction_image(example_image, model, test_transforms, device)
-    st.sidebar.write(f"Predicted class: {predicted_class}")
-    st.sidebar.write(f"Confidence: {confidence}")
+        # Display example image
+        example_image = Image.open(selected_example_media[1])
+        st.sidebar.image(example_image, caption='Example Image', use_column_width=True)
+
+        # Make prediction on example image
+        predicted_class, confidence = make_prediction_image(example_image, model, test_transforms, device)
+        st.sidebar.write(f"Predicted class: {predicted_class}")
+        st.sidebar.write(f"Confidence: {confidence}")
+
+    elif selected_media_type == "Video":
+        selected_example_media = st.sidebar.selectbox("Select an example video", example_videos, format_func=lambda x: x[0])
+
+        # Convert uploaded video to MP4 format
+        # video_path = convert_to_mp4(open(selected_example_media[1], 'rb'))
+        video_path = selected_example_media[1]
+
+        # Display converted video
+        # st.video(open(video_path, 'rb'), format='video/mp4')
+
+        # Process video and display frames with high confidence
+        process_video(video_path, model, test_transforms, device)
 
 
 if __name__ == "__main__":
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -36,3 +36,55 @@ def format_filename(filename):
     name_parts = os.path.splitext(filename)[0].split('_')
     camel_case_name = ' '.join([part.capitalize() for part in name_parts])
     return camel_case_name
+
+# Function to convert OpenCV frame to PIL Image
+def get_pil_image_from_frame(frame):
+    return Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+
+# Function to convert video to MP4 format using ffmpeg
+def convert_to_mp4(video_file):
+    # Write video content to a temporary file
+    temp_video_file = tempfile.NamedTemporaryFile(delete=False, suffix='.avi')
+    temp_video_file.write(video_file.read())
+    temp_video_file.close()
+
+    # Define output file path for MP4 file
+    output_file_path = tempfile.mktemp(suffix='.mp4')
+
+    # Run ffmpeg command for conversion
+    command = ['ffmpeg', '-i', temp_video_file.name, '-codec:v', 'libx264', '-codec:a', 'aac', '-strict', 'experimental', output_file_path]
+    subprocess.run(command)
+
+    # Clean up temporary video file
+    os.remove(temp_video_file.name)
+
+    return output_file_path
+
+
+# Function to process video
+def process_video(video_path, model, test_transforms, device): 
+    # Load the video capture
+    video_capture = cv2.VideoCapture(video_path)
+    frame_rate = video_capture.get(cv2.CAP_PROP_FPS)
+
+    frame_number = 0
+    while video_capture.isOpened():
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+
+        frame_number += 1
+
+        pil_image = get_pil_image_from_frame(frame)
+        # Make prediction on the frame
+        predicted_class, confidence = make_prediction_image(pil_image, model, test_transforms, device)
+
+        print(predicted_class, confidence)
+
+        if confidence >= Config.CONFIDENCE_THRESHOLD:
+            # Display frame with high confidence
+            st.image(pil_image, caption=f"Frame {frame_number}: {predicted_class} (Confidence: {confidence})", use_column_width=True)
+
+    # Release video capture
+    video_capture.release()
