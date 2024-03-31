e93ea4b9ea5907065417e0ba882b8f0340ea4699
meetpatel0963 <18bce164@nirmauni.ac.in>
2024-03-31 16:59:53 +0530

    Added feature to visualize XAI explanation heatmaps for image predictions

--- app/app.py
+++ app/app.py
@@ -10,8 +10,8 @@ from PIL import Image
 
 from config import Config
 from models.resnet50 import ResNet50Classifier
-from utils.utils import make_prediction_image, get_pil_image_from_frame, \
-                        format_filename, convert_to_mp4, process_video
+from utils.utils import make_prediction_image, get_pil_image_from_frame, explain_prediction_image, \
+                        visualize_explanation_heatmaps, format_filename, convert_to_mp4, process_video
 
 
 use_cuda = torch.cuda.is_available()
@@ -35,18 +35,18 @@ def main():
     uploaded_file = st.file_uploader("Upload an image or video", type=['jpg', 'jpeg', 'png', 'mp4', 'avi'])
 
     # Example images
-    example_images = []
+    example_images = [(None, None)]
     for filename in os.listdir(Config.SAMPLE_IMAGES_PATH):
         if os.path.isfile(os.path.join(Config.SAMPLE_IMAGES_PATH, filename)):
             example_images.append((format_filename(filename), os.path.join(Config.SAMPLE_IMAGES_PATH, filename)))  # Store tuple of (label, path)
 
     # Example videos
-    example_videos = []
+    example_videos = [(None, None)]
     for filename in os.listdir(Config.SAMPLE_VIDEOS_PATH):
         if os.path.isfile(os.path.join(Config.SAMPLE_VIDEOS_PATH, filename)):
             example_videos.append((format_filename(filename), os.path.join(Config.SAMPLE_VIDEOS_PATH, filename)))  # Store tuple of (label, path)
 
-    if uploaded_file is not None:
+    if uploaded_file is not None: 
         file_extension = uploaded_file.name.split('.')[-1].lower()
         if file_extension in ['mp4', 'avi']:
             if file_extension == 'avi':
@@ -57,44 +57,67 @@ def main():
                 # Process video and display frames with high confidence
                 process_video(video_path, model, test_transforms, device)
         else:
-            # Display uploaded image
-            st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
-
             # Convert uploaded image to PIL Image
             pil_image = Image.open(uploaded_file)
 
-            # Make prediction on the image
-            predicted_class, confidence = make_prediction_image(pil_image, model, test_transforms, device)
-            st.write(f"Predicted class: {predicted_class}")
+            # Display uploaded image
+            st.image(pil_image, caption='Uploaded Image', width=300)
+
+            # Make prediction
+            predicted_class, predicted_class_name, confidence = make_prediction_image(pil_image, model, test_transforms, device)
+
+            # Display predicted class and confidence
+            st.write(f"Predicted class: {predicted_class_name}")
             st.write(f"Confidence: {confidence}")
 
-    st.sidebar.title("Example Media")
-    selected_media_type = st.sidebar.radio("Select media type", ["Image", "Video"])
+            if st.button("Show Visualizations"):
+                # Explain prediction on uploaded image using XAI techniques
+                gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask = \
+                            explain_prediction_image(pil_image, model, test_transforms, predicted_class, device)
 
-    if selected_media_type == "Image":
-        selected_example_media = st.sidebar.selectbox("Select an example image", example_images, format_func=lambda x: x[0])
+                # Visualize explanation heatmaps
+                visualize_explanation_heatmaps(gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask) 
+    
+    else: 
+        st.sidebar.title("Example Media")
+        selected_media_type = st.sidebar.radio("Select media type", ["Image", "Video"])
 
-        # Display example image
-        example_image = Image.open(selected_example_media[1])
-        st.sidebar.image(example_image, caption='Example Image', use_column_width=True)
+        if selected_media_type == "Image":
+            selected_example_media = st.sidebar.selectbox("Select an example image", example_images, index=0, format_func=lambda x: x[0])
 
-        # Make prediction on example image
-        predicted_class, confidence = make_prediction_image(example_image, model, test_transforms, device)
-        st.sidebar.write(f"Predicted class: {predicted_class}")
-        st.sidebar.write(f"Confidence: {confidence}")
+            if selected_example_media[1] is not None: 
+                # Display example image
+                example_image = Image.open(selected_example_media[1])
+                st.sidebar.image(example_image, caption='Example Image', use_column_width=True)
 
-    elif selected_media_type == "Video":
-        selected_example_media = st.sidebar.selectbox("Select an example video", example_videos, format_func=lambda x: x[0])
+                # Make prediction
+                predicted_class, predicted_class_name, confidence = make_prediction_image(example_image, model, test_transforms, device)
 
-        # Convert uploaded video to MP4 format
-        # video_path = convert_to_mp4(open(selected_example_media[1], 'rb'))
-        video_path = selected_example_media[1]
+                # Display predicted class and confidence
+                st.sidebar.write(f"Predicted class: {predicted_class_name}")
+                st.sidebar.write(f"Confidence: {confidence}")
 
-        # Display converted video
-        # st.video(open(video_path, 'rb'), format='video/mp4')
+                if st.button("Show Visualizations"):
+                    # Explain prediction on uploaded image using XAI techniques
+                    gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask = \
+                                explain_prediction_image(example_image, model, test_transforms, predicted_class, device)
 
-        # Process video and display frames with high confidence
-        process_video(video_path, model, test_transforms, device)
+                    # Visualize explanation heatmaps
+                    visualize_explanation_heatmaps(gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask) 
+
+        elif selected_media_type == "Video":
+            selected_example_media = st.sidebar.selectbox("Select an example video", example_videos, index=0, format_func=lambda x: x[0])
+
+            if selected_example_media[1] is not None: 
+                # Convert uploaded video to MP4 format
+                # video_path = convert_to_mp4(open(selected_example_media[1], 'rb'))
+                video_path = selected_example_media[1]
+
+                # Display converted video
+                # st.video(open(video_path, 'rb'), format='video/mp4')
+
+                # Process video and display frames with high confidence
+                process_video(video_path, model, test_transforms, device)
 
 
 if __name__ == "__main__":
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -6,10 +6,38 @@ import os
 import cv2
 from PIL import Image
 
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from skimage.segmentation import mark_boundaries
+
 import tempfile
 import subprocess
 
 from config import Config
+from xai.GradientCAM import GradientCAM
+from xai.SaliencyMap import SaliencyMap
+from xai.IntegratedGradients import IntegratedGradients
+from xai.Lime import Lime
+from xai.XAITechniques import XAITechniques
+
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from captum.attr import Saliency
+from lime import lime_image
+
+
+default_cmap = LinearSegmentedColormap.from_list('custom blue',
+                                                 [(0, '#ffffff'),
+                                                  (0.25, '#000000'),
+                                                  (1, '#000000')], N=256)
+
+
+def min_max_normalize(heatmap):
+    min_value = heatmap.min()
+    max_value = heatmap.max()
+    normalized_heatmap = (heatmap - min_value) / (max_value - min_value)
+    return normalized_heatmap
 
 
 # Function to make predictions on an image
@@ -24,10 +52,63 @@ def make_prediction_image(image, model, test_transforms, device):
         predicted_class = torch.argmax(probabilities).item()
         confidence = probabilities[predicted_class].item()
 
-    predicted_class = Config.CLASS_NAMES[predicted_class]
+    predicted_class_name = Config.CLASS_NAMES[predicted_class]
     confidence = round(confidence, 3)
 
-    return predicted_class, confidence
+    return predicted_class, predicted_class_name, confidence
+
+
+def explain_prediction_image(image, model, test_transforms, predicted_class, device):
+    # Apply transforms to image
+    transformed_image = test_transforms(image)
+
+    # Initialize XAI techniques
+    xai_techniques = XAITechniques(
+        GradientCAM(GradCAM(model=model, target_layers=[model.resnet50[7][-1]])),
+        SaliencyMap(saliency=Saliency(model)), 
+        IntegratedGradients(model),
+        Lime(explainer=lime_image.LimeImageExplainer(), model=model, device=device)
+    )
+
+    # Run XAI techniques
+    gradcam, gradcam_visualization = xai_techniques.run_gradcam(transformed_image.unsqueeze(0).to(device), target=predicted_class)
+    saliency_map, saliency_map_visualization = xai_techniques.run_saliency_map(transformed_image.unsqueeze(0).to(device), target=predicted_class)
+    integrated_gradients = xai_techniques.run_integrated_gradients(transformed_image.unsqueeze(0).to(device), predicted_class)
+    lime_heatmap, lime_segments, lime_image_map, lime_mask = xai_techniques.run_lime(transformed_image)
+
+    return gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask
+
+
+def visualize_explanation_heatmaps(gradcam_visualization, saliency_map_visualization, integrated_gradients, lime_heatmap, lime_image_map, lime_mask): 
+    # Display explanation heatmaps
+    fig, axes = plt.subplots(2, 3, figsize=(20, 12), dpi=600, gridspec_kw={'wspace': 0.1, 'hspace': 0.1})
+
+    axes[0, 0].imshow(gradcam_visualization)
+    axes[0, 0].set_title('GradCAM', fontsize=22)
+    axes[0, 0].axis('off')
+
+    axes[0, 1].imshow(saliency_map_visualization)
+    axes[0, 1].set_title('Saliency Map', fontsize=22)
+    axes[0, 1].axis('off')
+
+    axes[0, 2].imshow(integrated_gradients, cmap=default_cmap, alpha=0.7)
+    axes[0, 2].set_title('Integrated Gradients', fontsize=22)
+    axes[0, 2].axis('off')
+
+    axes[1, 0].imshow(mark_boundaries(lime_image_map, lime_mask))
+    axes[1, 0].set_title('LIME: Top 3 Superpixels', fontsize=22)
+    axes[1, 0].axis('off')
+
+    im = axes[1, 1].imshow(lime_heatmap, cmap='RdBu', vmin=-lime_heatmap.max(), vmax=lime_heatmap.max())
+    axes[1, 1].set_title('LIME', fontsize=22)
+    cax = fig.add_axes([axes[1, 1].get_position().x1 + 0.01, axes[1, 1].get_position().y0, 0.01, axes[1, 1].get_position().height])
+    cbar = plt.colorbar(im, cax=cax)
+    axes[1, 1].axis('off')
+
+    # Hide the last subplot as it's empty in a 2x3 layout
+    axes[1, 2].axis('off')
+
+    st.pyplot(fig, use_container_width=True)
 
 
 # Function to format filename
@@ -77,9 +158,10 @@ def process_video(video_path, model, test_transforms, device):
         frame_number += 1
 
         pil_image = get_pil_image_from_frame(frame)
+
         # Make prediction on the frame
         predicted_class, confidence = make_prediction_image(pil_image, model, test_transforms, device)
-
+        
         print(predicted_class, confidence)
 
         if confidence >= Config.CONFIDENCE_THRESHOLD:
